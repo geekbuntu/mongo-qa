8b0b1e86e2a1e199031d791114b5f35f253c9c99
kristina <k@wooster.earthlink.net>
2009-01-15 12:07:55 -0500

    refactored

--- modules/driver_test_framework/tester.py
+++ modules/driver_test_framework/tester.py
@@ -4,109 +4,192 @@ import sys
 import subprocess
 from datetime import datetime
 
+ID = str( datetime.now() )
 
-def process_output( true_out , out , report , time ):
-    report.write( "RESULTS FOR " + test + "\n\tRUN BY: " + driver_name + "\n\tDATE: " + time[ "id" ] + "\n\n" )
+TEST_DIR = "/src"
+VALIDATION_DIR = "/validate"
+OUTPUT_DIR = "/out"
+TEMP_FILE = "/temp_" + ID
 
-    # create diff
-    count = 0
-    diffcount = 0
-    for line1 in true_out:
-        line2 = out.readline()
-        if line2 != line1 :
-            report.write( "L" + str(count) + ":\n" )
-            report.write( "> " + line1 + "\n" )
-            report.write( "< " + line2 + "\n" )
-            diffcount += 1
-        count += 1
+class Renderer :
+    def __init__(self, output, p_output, report):
+        if os.path.exists( TEMP_FILE ):
+            self.temp = open( TEMP_FILE, 'r' )
+        else:
+            self.temp = None
+        if os.path.exists( output ):
+            self.output = open( output, 'r' )
+        else:
+            self.output = None
+        if os.path.exists( p_output ):
+            self.p_output = open( p_output, 'r' )
+        else:
+            self.p_output = None
+                
+        self.report = open( report, 'w' )
 
-    report.write( "\t# incorrect output lines: " + str(diffcount) +"\n\n" )
+    def render_header( self, test, driver ):
+        self.report.write( "RESULTS FOR " + test + "\n" )
+        self.report.write( "\tRUN BY: " + driver + "\n" )
+        self.report.write( "\tDATE: " + ID + "\n\n" )
+
+    def render_diff( self ):
+        if not self.p_output:
+            return
+
+        # create diff
+        diffcount = 0
+        for line1 in self.p_output:
+            line2 = self.output.readline()
+            if line2 != line1 :
+                self.report.write( "L" + str(count) + ":\n" )
+                self.report.write( "> " + line1 + "\n" )
+                self.report.write( "< " + line2 + "\n" )
+                diffcount += 1
+
+        self.report.write( "\t# incorrect output lines: " + str(diffcount) +"\n\n" )
 
     # gather reporting info at the end of the output
-    extra = {}
-    for line2 in out:
-        temp = line2.split( ":" )
-        if len( temp ) < 2:
-            continue
-        extra[ temp[0].lstrip() ] = temp[1].lstrip()
+    def get_stats( self ):
+        extra = {}
+        if not self.output:
+            return extra
 
-    return extra
-
-
-def report_summary( report, time, stats, exit_code ):
-
-    report.write( "measured by tester.py: \n" )
-    report.write( "\tstarted:  " + str(time[ "begin" ]) + "\n" )
-    report.write( "\tfinished: " + str(time[ "end" ]) + "\n" )
-    report.write( "\tdiff:     " + str(time[ "end" ] - time[ "begin" ]) + "\n\n" )
-    report.write( "\texit code: " + str(exit_code) +"\n\n" )
-
-    report.write( "measured by driver: \n" )
-    report.write( "\tstarted:  " + stats[ "begintime" ] + "\n" )
-    report.write( "\tfinished: " + stats[ "endtime" ] + "\n" )
-    report.write( "\tdiff:     " + stats[ "totaltime" ] + "\n\n" )
-    report.write( "\texecuted ok: " + stats[ "ok" ] +"\n\n" )
+        for line in self.output:
+            temp = line.split( ":" )
+            if len( temp ) < 2:
+                continue
+            extra[ temp[0].lstrip() ] = temp[1].lstrip()
+            
+        return extra
 
 
-def report_validation( report, exit_code, temp ):
-    if exit_code == 0:
-        report.write( "\n\nPASSED VALIDATION\n" )
-    else:
-        report.write( "\n\nFAILED VALIDATION\n" )
-        temp.close();
-        temp = open( "temp", 'r' )
-        for line in temp:
-            report.write( line );
+    def render_stats( self, result ):
+        self.report.write( "measured by tester.py: \n" )
+        self.report.write( "\tbegintime:  " + str(result[ "begin" ]) + "\n" )
+        self.report.write( "\tendtime: " + str(result[ "end" ]) + "\n" )
+        self.report.write( "\ttotaltime:     " + str(result[ "end" ] - result[ "begin" ]) + "\n\n" )
+        self.report.write( "\tok: " + str(result["exit_code"]) +"\n\n" )
+        
+        stats = self.get_stats()
+        self.report.write( "measured by driver: \n" )
+        for i in [ "begintime", "endtime", "totaltime", "ok" ]:
+            if "begintime" in stats:
+                self.report.write( "\t" + i + ":  " + stats[ i ] + "\n" )
+            else:
+                self.report.write( "\t" + i + ":  --no " + i + " recorded--\n" )
 
 
-test_dir = os.curdir + "/cmd_tests"
-tests = os.listdir( test_dir + "/src" )
+    def render_validation( self, result ):
+        if not result: 
+            self.report.write( "\n\nERROR!  COULD NOT RUN VALIDATE!\n" )
+        elif result[ "exit_code" ] == 0:
+            self.report.write( "\n\nPASSED VALIDATION\n" )
+        else:
+            self.report.write( "\n\nFAILED VALIDATION\n" )
+            for line in self.temp:
+                self.report.write( line )
 
-for driver in sys.argv[1:]:
-    try:
-        temp = driver.split( "=" )
-        driver_name = temp[0]
-        driver_path = temp[1]
-    except IndexError:
-        print "improperly formatted argument: " + driver + ".  skipping..."
-        continue
 
-    local_test_dir = test_dir + "/" + driver_name
-    if not os.path.exists( local_test_dir ):
-        os.mkdir( local_test_dir )
+    def cleanup( self ):
+        if self.temp:
+            self.temp.close()
+        if os.path.exists( TEMP_FILE ):
+            os.remove( TEMP_FILE )
+        if self.output:
+            self.output.close()
+        if self.p_output:
+            self.p_output.close()
+        if self.report:
+            self.report.close()
 
-    for test in tests:
-        t = { "id" : str( datetime.now() ) }
-        test_path = local_test_dir + "/" + test[0:-4] + "_" + t[ "id" ]
 
-        # files
-        out = open( test_path + ".out" , 'w' )
-        report = open( test_path + ".report", 'w' )
+class Driver:
+    def __init__( self, d ):
+        try:
+            temp = d.split( "=" )
+            self.name = temp[0]
+            self.path = temp[1]
+        except IndexError:
+            print "improperly formatted argument: " + driver + ".  skipping..."
 
+    def is_valid( self ):
+        return self.name != None and self.path != None
+
+    def get_dir( self, test_dir ):
+        dir = test_dir + "/" + self.name
+        if not os.path.exists( dir ):
+            os.mkdir( dir )
+        return dir
+
+    def get_path( self ):
+        return self.path
+
+    def get_name( self ):
+        return self.name
+
+    def get_unique_path( self, test_dir, test ):
+        # a unique location for this driver/test/run's files:
+        #     TEST_DIR/driver_name/test_ID
+        driver_dir = self.get_dir( test_dir );
+        return driver_dir + "/" + test + "_" + ID
+
+
+class Framework:
+    def __init__( self ):
+        self.test_dir = os.curdir + TEST_DIR
+        self.tests = os.listdir( self.test_dir + VALIDATION_DIR )
+
+    # run all tests on all drivers
+    def run_all( self ):
+        for d in sys.argv[1:]:
+            driver = Driver( d )
+            if not driver.is_valid():
+                continue
+
+            self.run_all_driver( driver );
+
+    # run all tests on a given driver
+    def run_all_driver( self, driver ):
+        for test in self.tests:
+            self.run_test( driver, test )
+
+    # run a specific test on a given driver
+    def run_test( self, driver, test ):
+        # file locations
+        out = driver.get_unique_path( self.test_dir, test ) + ".out"
+        report = driver.get_unique_path( self.test_dir, test ) + ".report"
+        perfect_out = self.test_dir + OUTPUT_DIR + "/" + test + ".out"
+        
         # run test
-        t[ "begin" ] = datetime.now()
-
-        exit_code = subprocess.call( [driver_path, test], 0, None, None, out )
-
-        t[ "end" ] = datetime.now()
-
-        # report output
-        out.close();
-        out = open( test_path + ".out" , 'r' )
-        true_out = open( test_dir + "/src/" + test , "r" )
-        stats = process_output( true_out , out , report , t )
-        report_summary( report, t, stats, exit_code )
+        timing_result = self.run_timed_test( driver, test, out, {} );
 
         # validate
-        temp = open( "temp", 'w' )
-        validate_script = test_dir + "/validate/" + test[0:-4]
-        print validate_script
-        exit_code = subprocess.call( [ validate_script ], 0, None, None, temp )
-        report_validation( report, exit_code, temp )
+        validation_result = None
+        try:
+            validation_result = self.run_validation_test( test, {} )
+        except OSError:
+            print "OSError: are the permissions correct for " + VALIDATION_DIR + "/" + test + "?\n"
 
-        # finish up
-        temp.close()
-        os.remove( "temp" )
-        report.close()
-        out.close()
+        # report output
+        r = Renderer( out, perfect_out, report )
+        r.render_header( test, driver.get_name() )
+        r.render_diff()
+        r.render_stats( timing_result )
+        r.render_validation( validation_result )
+        r.cleanup()
 
+    def run_timed_test( self, driver, test, output, result ):
+        result[ "begin" ] = datetime.now()
+        result[ "exit_code" ] = subprocess.call( [driver.get_path(), test, output] )
+        result[ "end" ] = datetime.now()
+        return result
+
+    def run_validation_test( self, test, result ):
+        validate_script = self.test_dir + "/validate/" + test
+        result[ "exit_code" ] = subprocess.call( [ validate_script, TEMP_FILE ] )
+        return result
+
+
+f = Framework()
+f.run_all()
