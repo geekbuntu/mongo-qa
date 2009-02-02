6ca3e48fb64838cc967f9885747242c4003a5b3c
Geir Magnusson Jr <geir@geir-magnusson-jrs-mac-pro.local>
2009-02-02 09:04:04 -0500

    Make sure the various parts of the distribution build loop on git as well

---
+++ modules/dist_build/loop_git.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+#  github has it's bad days - so lets just do several tries
+#
+
+for i in 1 2 3 4 5 ; do
+  echo "Git attempt $i"
+  git $@
+  if [ $? -eq 0 ]; then
+    break
+  else
+    sleep 2
+  fi
+done
+
