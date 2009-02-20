55f790dabeb1b2f8cc459ccd1285529fb6938166
Geir Magnusson Jr <geir@pobox.com>
2009-02-20 16:01:27 -0500

    Ensure that any docs that are css get uploaded to S3 w/ text/css content type

--- modules/dist_build/upload_docs_s3.sh
+++ modules/dist_build/upload_docs_s3.sh
@@ -9,7 +9,14 @@
 
 for j in `cd $1; find . -type f | sed -e "s/.\///"` 
 do
-   echo "Uploading : $1/$j to $2/$j"
-   ~/s3curl.pl --id=xgen --put=$1/$j --contentType text/html --acl public-read --  http://api.mongodb.org.s3.amazonaws.com/$2/$j
+   contentType="text/html"
+
+   if [ ${j##*.} == "css" ]
+   then
+       contentType="text/css"
+   fi
+
+   echo "Uploading : $1/$j to $2/$j as contentType = $contentType"
+   ~/s3curl.pl --id=xgen --put=$1/$j --contentType $contentType --acl public-read --  http://api.mongodb.org.s3.amazonaws.com/$2/$j
 done
 
